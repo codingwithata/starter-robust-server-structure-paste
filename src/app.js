eac56d292f086aa2dfca267e6d77c1d5dd05d395
Malik <Mata@indeed.com>
2024-01-19 10:05:05 -0800

    Add routes to get JSon data

--- src/app.js
+++ src/app.js
@@ -3,9 +3,26 @@ const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
+const pastes = require("./data/pastes-data");
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
+});
+
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
 });
 
 // Error handler
